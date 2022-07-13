52b4911d0f5da81051566bdeddc961f94c54fd92
zzchr1s <56876680+zzchr1s@users.noreply.github.com>
2022-07-13 14:27:09 +0800

    second commit

--- R/RefTM.R
+++ R/RefTM.R
@@ -3,12 +3,14 @@ RefTM <- function(sc_data, ref_data, k1 = 5, k2 = NULL, workflow = "LDA", covari
   if(workflow == "LDA"){
     model_ref <- RefTM.LDA(ref_data, k = k1, k0 = 0, bulk_beta = NULL)
     print("Finished modeling the reference data.")
+    print(k2)
     if(!is.null(k2)){
+      print(k2)
       sprintf("Number of topics is fixed to be %s.", k1+5)
-      model_sc <- RefTM.LDA(sc_data, k = k1+5, method = "VEM", k0 = k1, bulk_beta = model_ref@beta, control = list(estimate.alpha = F))
+      model_sc <- RefTM.LDA(t(sc_data), k = k1+k2, method = "VEM", k0 = k1, bulk_beta = model_ref@beta, control = list(estimate.alpha = F))
     }else{
       print("Select the best model.")
-      k <- k1 + c(5,10,15,20,25)
+      k <- k1 + c(0,2,5,10,15,20)
       models <- list()
       perplex <- c()
       for (i in 1:5) {
@@ -21,7 +23,7 @@ RefTM <- function(sc_data, ref_data, k1 = 5, k2 = NULL, workflow = "LDA", covari
       par(bty = 'n')
       plot(log.lik$topics, log.lik$LL, xlab="Number of topics", ylab="perplexity", type='o', pch=16, col='black', main='Model selection')
       points(log.lik$topics[which(log.lik$second_derivative == max(log.lik$second_derivative))], log.lik$LL[which(log.lik$second_derivative == max(log.lik$second_derivative))], pch=4, col='red', lwd = 7)
-      model_sc <- models[(which(log.lik$second_derivative == max(log.lik$second_derivative)) - k1)/5]
+      model_sc <- models[which(log.lik$second_derivative == max(log.lik$second_derivative))]
     }
   }else
 
@@ -44,17 +46,17 @@ RefTM <- function(sc_data, ref_data, k1 = 5, k2 = NULL, workflow = "LDA", covari
     print("Finished modeling the reference data.")
     if(!is.null(k2)){
       sprintf("Number of topics is fixed to be %s.", k1+5)
-      model_sc <- RefTM.STM(getCorpus(sc_data), as.character(1:dim(sc_data)[2]), K = k1+5, prevalence = ~covariate, refBeta = exp(model_ref$beta$logbeta[[1]]))
+      model_sc <- RefTM.STM(getCorpus(t(sc_data)), as.character(1:dim(sc_data)[1]), K = k1+5, prevalence = ~covariate, refBeta = exp(model_ref$beta$logbeta[[1]]))
     }else{
       print("Select the best model.")
       perp = function(model){
         exp(-model$convergence$bound[model$convergence$its]/sum(model$settings$dim$wcounts$x))
       }
-      k <- k1 + c(5,10,15,20,25)
+      k <- k1 + c(0,2,5,10,15,20)
       models <- list()
       perplex <- c()
       for (i in 1:5) {
-        models[i] <- RefTM.STM(getCorpus(sc_data), as.character(1:dim(sc_data)[2]), K = k[i], prevalence = ~covariate, refBeta = exp(model_ref$beta$logbeta[[1]]))
+        models[i] <- RefTM.STM(getCorpus(t(sc_data)), as.character(1:dim(sc_data)[1]), K = k[i], prevalence = ~covariate, refBeta = exp(model_ref$beta$logbeta[[1]]))
         perplex[i] <- perp(models[i])
       }
       log.lik <- data.frame(topics = k, LL = perplex)
@@ -63,10 +65,10 @@ RefTM <- function(sc_data, ref_data, k1 = 5, k2 = NULL, workflow = "LDA", covari
       par(bty = 'n')
       plot(log.lik$topics, log.lik$LL, xlab="Number of topics", ylab="perplexity", type='o', pch=16, col='black', main='Model selection')
       points(log.lik$topics[which(log.lik$second_derivative == max(log.lik$second_derivative))], log.lik$LL[which(log.lik$second_derivative == max(log.lik$second_derivative))], pch=4, col='red', lwd = 7)
-      model_sc <- models[(which(log.lik$second_derivative == max(log.lik$second_derivative)) - k1)/5]
+      model_sc <- models[which(log.lik$second_derivative == max(log.lik$second_derivative))]
     }
   }
-  return(model_sc)
+  return(model_sc = model_sc)
 }
 
 
