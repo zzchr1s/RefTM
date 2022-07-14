844ac9100d4bebc09bf1479bf421305bcb27cde8
zzchr1s <56876680+zzchr1s@users.noreply.github.com>
2022-07-14 21:19:34 +0800

    commit

--- R/RefTM_tsne.R
+++ R/RefTM_tsne.R
@@ -5,14 +5,14 @@ RefTM_tsne = function(mat,label,donor = NULL,title = NULL,seed = 1,perplexity = 
   if(is.null(donor) == TRUE){
     tsne.data = data.frame(label = label,tsne.info$Y)
     p = ggpubr::ggscatter(tsne.data,title = title,x = "tSNE_1", y = "tSNE_2",color = "label",size = size,xlab = NULL, ylab = NULL,palette = palette)+
-      ggthemes::theme_base() + ggplot2::theme(plot.title = element_text(hjust = 0.5))+ ggplot2::theme(legend.position="left")
+      ggthemes::theme_base() + ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))+ ggplot2::theme(legend.position="left")
 
     if(legend == FALSE) p = p + ggplot2::theme(legend.position="none")
   }
   if(is.null(donor) == FALSE){
     tsne.data = data.frame(label = label,donor = donor, tsne.info$Y)
     p = ggpubr::ggscatter(tsne.data,title = title,x = "tSNE_1", y = "tSNE_2",color = "label",shape = "donor",size = size,xlab = NULL
-                  , ylab = NULL,palette = palette)+ ggplot2::scale_shape_manual(values=c(16,8)) + ggthemes::theme_base() + ggplot2::theme(plot.title = element_text(hjust = 0.5))+
+                  , ylab = NULL,palette = palette)+ ggplot2::scale_shape_manual(values=c(16,8)) + ggthemes::theme_base() + ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))+
       ggplot2::theme(legend.position="left")
     if(legend == FALSE) p = p + ggplot2::theme(legend.position="none")
   }
